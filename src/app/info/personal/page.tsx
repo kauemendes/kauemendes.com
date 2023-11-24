426cafc1c92a7d8df6a83f46c35d1fb56636328d
ALM TEAM <alm@millenniumbcp.net>
2023-11-24 18:02:41 +0000

    Added Personal Page as Markdown

--- src/app/info/personal/page.tsx
+++ src/app/info/personal/page.tsx
@@ -1,70 +1,17 @@
 /* eslint-disable  */
 import Heading from "@/components/Heading";
+import { getContent } from "@/lib/content";
 
-export default function PersonalPage() {
+export default async function PersonalPage() {
+  const person = await getContent('personal');
   return (
     <>
-      <Heading>Personal Page</Heading>
-      <div className='w-2/4 m-auto text-justify'>
-        <p>
-
-          I am an incredibly passionate individual dedicated to crafting code that transforms people's lives, whether through automation, innovative products, or impactful pull requests.
-        </p>
-        <br />
-        <p>
-          I firmly believe that technology has the power to effectively influence everyone's life for the better. Technology and collaboration go hand in hand, and that's precisely what I strive to bring wherever I go.
-        </p>
-        <br />
-        <p>
-          I hope that together we can contribute to building a better world!
-        </p>
-        <br />
-        <p>
-          I was born in 1989 in the small city of Mogi das Cruzes, São Paulo. Growing up in the late '80s, I have always been a very curious individual, particularly about technology. Around the age of 8 or 9, my family acquired our first computer, a Pentium 133.
-        </p>
-        <p>
-          This was a significant change in my life; it was the moment I discovered the world. Soon after, I eagerly sought to learn even more. At the age of 11, I enrolled in my first computer course, covering topics such as the Microsoft Office suite, typing skills, and web usage.
-        </p>
-        <br />
-        <p className="text-center"> ~~~ </p>
-        <br />
-        <p>
-          My first foray into web development was with FrontPage, where websites were constructed as large tables in the early versions. Essentially, a page was a table within a table within a table.
-        </p>
-        <p>
-          I spent weekends delving into the source codes of websites, striving to comprehend how I could create my own projects. As a result, the desire to learn more and more became insatiable. The next step was a course focused specifically on Web Design, where I gained exposure to the early versions of Photoshop, Macromedia Flash, Dreamweaver, and other tools.
-        </p>
-        <p>
-          I delved deeper into understanding how protocols and the then-nascent, scarcely explored realm of high-speed internet were shaping up in the early 2000s.
-        </p>
-        <br />
-        <p className="text-center"> ~~~ </p>
-        <br />
-        <p>
-          After much exploration, I encountered my first small projects around the years 2003/2004, precisely when personal blogs began to boom. People sought customized templates, and many individuals were selling them online. I decided to dive into this trend and ended up creating templates that I could take pride in. I demystified blog engines, understanding how they functioned, often keeping me awake into the early hours of the morning.
-        </p>
-        <br />
-        <p>
-          I learned, I created, time passed, and then I realized it was crucial to truly learn to program, to write code—templates were no longer sufficient. That's when I had my first encounter with ASP.NET, creating dynamic pages with styling and incorporating some JavaScript.
-        </p>
-        <p>
-          The rest is history; I never stopped from there. I took a course on network security and hacking in corporate systems. Soon after, I enrolled in university, where I had the opportunity to absorb the best practices and academic insights.
-        </p>
-        <p>
-          I was already working from my first year in college, and the momentum never slowed. I began at the bottom—phone support, remote assistance, RedHat server installation and configuration. When I asked for a chance to start coding in the same company, there was no turning back.
-        </p>
-        <br />
-        <p className="text-center"> ~~~ </p>
-        <br />
-        <p>
-          I learned how real-life operates, how a team can be truly collaborative, and how the development world is a prime example of how collaboration among peers can genuinely elevate you. My learning style was hands-on, experimenting, and constantly creating my own labs. Always fueled by the curiosity of the beginning and an insatiable desire to learn more and more.
-        </p>
-        <p>
-          Now, living and working in a different country from where I grew up and learned, I've been gathering experience and insights across various realms of the tech world. My passion today is all about automating and letting code come to life, helping us tackle daily tasks and reach our goals.
-        <br></br>
-        <br></br>
-          What drives me now is creating a collaborative, agile, and diverse environment. Moving forward, my daily joy comes from assisting people with tasks, whether they're basic or exceptional. And I hope it stays this way—keeping things vibrant and exciting!
-        </p>
+      <Heading>{person.title}</Heading>
+      <div className={`bg-no-repeat bg-center bg-[url('/images/banner2.jpg')] w-full h-[100px]`} ></div>
+      <div className='max-w-md mx-auto overflow-hidden p-4 m-auto text-left md:max-w-3xl'>
+        <article dangerouslySetInnerHTML={{ __html: person.body }}
+          className="max-w-screen-md prose prose-slate"
+        />
       </div>
     </>
   );
---
+++ src/content/personal.md
@@ -0,0 +1,40 @@
+---
+title: "Personal Page"
+image: ""
+date: "2023-11-24"
+---
+I am an incredibly passionate individual dedicated to crafting code that transforms people's lives, whether through automation, innovative products, or impactful pull requests.
+
+I firmly believe that technology has the power to effectively influence everyone's life for the better. Technology and collaboration go hand in hand, and that's precisely what I strive to bring wherever I go.
+
+I hope that together we can contribute to building a better world!
+
+I was born in 1989 in the small city of Mogi das Cruzes, São Paulo. Growing up in the late '80s, I have always been a very curious individual, particularly about technology. Around the age of 8 or 9, my family acquired our first computer, a Pentium 133.
+
+This was a significant change in my life; it was the moment I discovered the world. Soon after, I eagerly sought to learn even more. At the age of 11, I enrolled in my first computer course, covering topics such as the Microsoft Office suite, typing skills, and web usage.
+
+---
+My first foray into web development was with FrontPage, where websites were constructed as large tables in the early versions. Essentially, a page was a table within a table within a table.
+
+
+I spent weekends delving into the source codes of websites, striving to comprehend how I could create my own projects. As a result, the desire to learn more and more became insatiable. The next step was a course focused specifically on Web Design, where I gained exposure to the early versions of Photoshop, Macromedia Flash, Dreamweaver, and other tools.
+
+I delved deeper into understanding how protocols and the then-nascent, scarcely explored realm of high-speed internet were shaping up in the early 2000s.
+
+---
+
+After much exploration, I encountered my first small projects around the years 2003/2004, precisely when personal blogs began to boom. People sought customized templates, and many individuals were selling them online. I decided to dive into this trend and ended up creating templates that I could take pride in. I demystified blog engines, understanding how they functioned, often keeping me awake into the early hours of the morning.
+
+I learned, I created, time passed, and then I realized it was crucial to truly learn to program, to write code—templates were no longer sufficient. That's when I had my first encounter with ASP.NET, creating dynamic pages with styling and incorporating some JavaScript.
+
+The rest is history; I never stopped from there. I took a course on network security and hacking in corporate systems. Soon after, I enrolled in university, where I had the opportunity to absorb the best practices and academic insights.
+
+I was already working from my first year in college, and the momentum never slowed. I began at the bottom—phone support, remote assistance, RedHat server installation and configuration. When I asked for a chance to start coding in the same company, there was no turning back.
+
+---
+
+I learned how real-life operates, how a team can be truly collaborative, and how the development world is a prime example of how collaboration among peers can genuinely elevate you. My learning style was hands-on, experimenting, and constantly creating my own labs. Always fueled by the curiosity of the beginning and an insatiable desire to learn more and more.
+
+Now, living and working in a different country from where I grew up and learned, I've been gathering experience and insights across various realms of the tech world. My passion today is all about automating and letting code come to life, helping us tackle daily tasks and reach our goals.
+
+What drives me now is creating a collaborative, agile, and diverse environment. Moving forward, my daily joy comes from assisting people with tasks, whether they're basic or exceptional. And I hope it stays this way—keeping things vibrant and exciting!
