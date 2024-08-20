f0b3980a8c26db235a25b04eb74dc5d0f8bd4899
Kaue Freitas <kaue.freitas@millenniumbcp.pt>
2024-08-20 17:47:49 +0100

    66 into next.js example

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,14 +2,6 @@ import Heading from "@/components/Heading";
 import { getPosts } from "@/lib/content";
 import Link from "next/link";
 
-export const metadata = {
-  title: 'Blog',
-  description: 'Personal Blog Kaue Mendes de Freitas.',
-  keywords: ['Kaue Mendes', 'Kaue Mendes de Freitas', 'Kaue Mendes Blog', 'Kaue Mendes Personal Blog', 'Kaue Mendes Blog'],
-  publisher: 'Kaue Mendes',
-  creator: 'Kaue Mendes',
-}
-
 export default async function BlogPage() {
   const posts = await getPosts();
   return (
@@ -17,11 +9,12 @@ export default async function BlogPage() {
       <div className="border-b">
         <Heading>Personal Blog</Heading>
       </div>
-      <div className="flex w-3/4 m-auto flex-wrap h-12 text-left sm:w-1/2">
+      <div className="flex w-3/4 m-auto flex-wrap text-left sm:w-1/2">
         { posts.map((post) => (
-          <div className="bg-stone-100 border w-full shadow hover:shadow-xl py-4 px-3 mt-1 rounded"  key={post.slug}>
+          <div className="bg-stone-100 border w-full shadow hover:shadow-xl py-4 px-3 mt-1 rounded gap-4"  key={post.slug}>
             <Link href={`/blog/${post.slug}`}>
-              <h2 className="text-stone-600 font-bold">{post.title}</h2>
+              <img src={post.image_banner} alt={post.title} width="320" height="180" className="rounded-t w-full h-32 object-cover rounded" />
+              <h1 className="text-center text-stone-900 font-bold mt-2">{post.title}</h1>
             </Link>
           </div>
         )) }
