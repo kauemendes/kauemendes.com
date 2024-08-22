03250c17efbe7d98c40506b2cc9f90cc9cfc59b9
Kaue Freitas <kaue.freitas@millenniumbcp.pt>
2024-08-22 09:16:25 +0000

    change themes for some values screens

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,13 @@ const config: Config = {
     './src/app/**/*.{jsx,tsx}',
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       fontFamily: {
         roboto: ['var(--font-roboto)', 'sans-serif'],
