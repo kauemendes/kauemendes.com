f5f61a0b9614a725a3dd47bf99fec852622880e2
Kaue Freitas <kaue.freitas@millenniumbcp.pt>
2024-08-22 08:49:47 +0000

    fix images for blog list

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -12,9 +12,9 @@ export default async function BlogPage() {
       </div>
       <div className="flex w-3/4 m-auto flex-wrap text-left sm:w-1/2">
         { posts.map((post, index) => (
-          <div className="bg-stone-100 border w-full shadow hover:shadow-xl py-4 px-3 mt-1 rounded gap-4"  key={post.slug}>
+          <div className="bg-stone-100 border w-full shadow hover:shadow-xl py-4 px-3 mt-1 rounded gap-4 bg-gradient-to-r from-cyan-400 to-blue-500"  key={post.slug}>
             <Link href={`/blog/${post.slug}`}>
-              <Image src={post.image_banner} alt={post.title} width={320} height={180} priority={index === 0} className="rounded-t w-full h-32 object-cover rounded" />
+              <Image src={post.image_banner} alt={post.title} width={320} height={180} priority={index === 0} className="rounded-t h-auto max-w-lg mx-auto" />
               <h1 className="text-center text-stone-900 font-bold mt-2">{post.title}</h1>
             </Link>
           </div>
