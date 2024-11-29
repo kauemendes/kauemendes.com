88fa4d8671fca214589356c7b51e334d3ea8544b
Kaue Freitas <kaue.freitas@millenniumbcp.pt>
2024-11-29 18:39:58 +0000

    redirects

--- next.config.js
+++ next.config.js
@@ -11,7 +11,29 @@ const images = {
       toRemotePattern(process.env.CMS_IMAGE_PATTERN)
     ],
   },
-  output: 'standalone'
+  output: 'standalone',
+  async redirects() {
+    return [
+      {
+        source: '/linkedin',
+        destination: 'https://www.linkedin.com/in/kauemendes/',
+        permanent: false,
+        basePath: false
+      },
+      {
+        source: '/whatsapp',
+        destination: 'https://wa.me/351965613249',
+        permanent: false,
+        basePath: false
+      },
+      {
+        source: '/github',
+        destination: 'https://github.com/kauemendes/',
+        permanent: false,
+        basePath: false
+      },
+    ]
+  },
 };
 
 module.exports = images;
