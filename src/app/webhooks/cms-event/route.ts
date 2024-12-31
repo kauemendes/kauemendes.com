c69cc559e06cb3adfa86736e3dacabf97b52dfc8
Kaue Freitas <kaue.freitas@millenniumbcp.pt>
2024-12-31 14:11:58 +0000

    rollback changge

--- src/app/webhooks/cms-event/route.ts
+++ src/app/webhooks/cms-event/route.ts
@@ -2,6 +2,7 @@ import { CACHE_TAG_POSTS } from "@/lib/content";
 import { revalidateTag } from "next/cache";
 
 export async function POST(request) {
+  const payload = await request.json();
   console.log('[GET]:', payload);
   if (payload.model === 'post') {
     revalidateTag(CACHE_TAG_POSTS);
