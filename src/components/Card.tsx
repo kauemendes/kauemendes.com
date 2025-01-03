c81c2af673b8267222a92af6cd1e2789e0359b29
Kaue Freitas <kaue.freitas@millenniumbcp.pt>
2025-01-03 11:58:12 +0000

    Fix Card Title

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -16,7 +16,7 @@ export default function Card({ title, body, image, url }: Card) {
         </Link>
         <div className="p-5">
             <Link href={url}>
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
             </Link>
             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{body}.</p>
             <Link href={url} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
